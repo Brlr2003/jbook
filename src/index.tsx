5a757b2db8ffb157e543d019b8b8d0189baab4dd
Brlr2003 <omaralsafarti03@gmail.com>
2023-09-20 21:49:38 +0200

    custom esbuild plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
-import ReactDOM from "react-dom";
-import { useState } from "react";
-
+import ReactDOM from "react-dom/client";
+import { useState, useEffect, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = () => {
-    console.log(input);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) return;
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
@@ -15,11 +37,13 @@ const App = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
-        <button>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
     </div>
   );
 };
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(
+  document.querySelector("#root") as HTMLElement
+);
+root.render(<App />);
