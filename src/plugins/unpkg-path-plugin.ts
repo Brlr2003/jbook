f2656989d797225a0e566018a3cbfded3599679d
Brlr2003 <omaralsafarti03@gmail.com>
2023-09-22 22:12:07 +0200

    caching using localForage

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,11 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
@@ -29,22 +35,35 @@ export const unpkgPathPlugin = () => {
       // LOADING
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
+
         if (args.path === "index.js") {
           return {
             loader: "jsx",
             contents: `
-            import React, {useState} from 'react';
+            import React, {useState} from 'react-select';
             console.log(React, useState);
             `,
           };
         }
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
         const { data, request } = await axios.get(args.path);
 
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
